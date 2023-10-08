092030c2478e0eab36e23d1d1d11f02ade1b2a5e
Evelyn Casillas <sistem.out.println.hello.evelyn@gmail.com>
2023-10-08 00:51:55 -0600

    se modifica documento html y js

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-function palabrasLargas(str, array) {
-    return array.filter(word => word.length > str.length);
-  }
-  
-  const miArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
-  console.log(palabrasLargas('bocina', miArrayArray)); 
-  // Output - ['insecto', 'bootcamp', 'escritorio']
+// Selecciona todos los elementos con la clase 'div' en el documento y los guarda en la variable 'list'
+const list = document.getElementsByClassName('div');
+// Un array de palabras
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+var array2 = [];
+// Función que depura strings
+function depuradorStrings(palabra, myArray) {
 
-
-  function printArray(array) {
-    const ul = document.createElement('ul');
-    array.forEach(word => {
-      const li = document.createElement('li');
-      li.textContent = word;
-      ul.appendChild(li);
+    myArray.forEach(element => {
+        if (palabra.length < element.length) {
+            console.log(element);
+            array2.push(element);
+            myFunction(element);
+        }
     });
-    document.body.appendChild(ul);
-  }
+}
+//depuradorStrings(palabra, myArray);
 
-  const myArray = ['insecto', 'bootcamp', 'escritorio'];
-printArray(myArray);
-// Output - <li>insecto</li> <li>bootcamp</li> <li>escritorio</li>
-//Falta modificar aún no funciona el código
+function myFunction(string) {
 
-  
+    var li = document.getElementById("li");
+    li.innerHTML += `<li>` + string + ` </li>`;
+
+}
 
 
+var palabraa = document.querySelector(".palabra")
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", function () {
+    depuradorStrings(palabraa.value, myArray);
+
+});
 
 /*Comentario de Kenia:realizó una breve descripción del ejercicio y explicaba con seguridad los pasos generales que realizaba, describía el por qué utilizaba tales constantes, parámetros y funciones. Hubieron algunos detalles con la utilización correcta de palabras reservadas pero su lógica de programación concluía muy bien, se necesita practicar un poco más con la resolución del problema, estuvo consciente del tiempo que se le fue proporcionado y está muy dispuesta a seguir aprendiendo. En general estuvo muy bien sus acciones tanto técnicas como blandas y manera de explicar y expresarse.
 */
